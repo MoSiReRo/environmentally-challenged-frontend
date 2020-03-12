b02a365b9087399905b1d440ac7a42dc72ea5d56
Rebecca French <rlfweb8@gmail.com>
2020-03-12 08:02:12 +0000

    graph component rendering progress circle.

--- src/Graph.js
+++ src/Graph.js
@@ -1,12 +1,38 @@
 import React from 'react';
-
+import { CircularProgressbar, buildStyles} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 class Graph extends React.Component {
 
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       CircularProgressbar
+    //     };
+    //   }
+
+    // Use props.minValue and props.maxValue to specify a range other than 0-100.
+
     render() {
+
+        const value = 0.25;
+
         return (
             <div>
-                Graph
+                <CircularProgressbar 
+                value={value} 
+                maxValue={90} 
+                text={`${value * 100}%`} 
+                styles={buildStyles({
+                    strokeLinecap: 'butt',
+                    textSize: '16px',
+                    pathTransitionDuration: 0.5,
+                    pathColor: 'red',
+                    textColor: 'blue',
+                    trailColor: 'green',
+                    backgroundColor: 'pink',
+                })}
+                    />; 
             </div>
         )
     }
