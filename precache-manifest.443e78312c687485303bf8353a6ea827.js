815a516c3c8b1fa14a0560999de7cbb181aa17ea
Rebecca French <rlfweb8@gmail.com>
2020-04-20 18:53:53 +0100

    Updates

---
+++ precache-manifest.443e78312c687485303bf8353a6ea827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45c0d2990bbade5e318096e18b8cfaa",
+    "url": "/environmentally-challenged-frontend/index.html"
+  },
+  {
+    "revision": "8a225b8d56939d2e9a9f",
+    "url": "/environmentally-challenged-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "09904d7163f8008a3e24",
+    "url": "/environmentally-challenged-frontend/static/js/2.6e672816.chunk.js"
+  },
+  {
+    "revision": "ddfd09a9397ea336a2550edb6da27f94",
+    "url": "/environmentally-challenged-frontend/static/js/2.6e672816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a225b8d56939d2e9a9f",
+    "url": "/environmentally-challenged-frontend/static/js/main.288cdad4.chunk.js"
+  },
+  {
+    "revision": "61162f1252f47d517129",
+    "url": "/environmentally-challenged-frontend/static/js/runtime-main.0f07b336.js"
+  }
+]);
